52b3b920e60284c836f4cbbce01108a31558f9e2
Joaqquinn <joaqu.gonzalezl@duocuc.cl>
2023-05-18 19:12:01 -0400

    se crea modelo de base de datos

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Pregunta(models.Model):
-    idPregunta = models.AutoField(primary_key=True,verbose_name='Codigo Pregunta')
-    descripcion = models.TextField()
 
 
 class Rol(models.Model):
@@ -18,10 +15,36 @@ class Usuario(models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
     rol = models.ForeignKey(Rol,on_delete=models.CASCADE,verbose_name='Rol')
-    pregunta = models.ForeignKey(Pregunta,on_delete=models.CASCADE,verbose_name='Pregunta')
-
-
-class Comentario (models.Model):
+    
+    
+class Publicacion(models.Model):
+    idPublicacion = models.AutoField(primary_key=True,verbose_name='Codigo Publicacion')
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=500)
+    fechaPublicacion = models.DateField(_(""), auto_now=False, auto_now_add=False)
+    estatus = models.CharField(max_length=50)
+    foto = models.ImageField()
+    idusuario =models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name='Id usuario')
+    
+    
+class Imagen(models.Model):
+    idImagen = models.AutoField(primary_key=True,verbose_name='Codigo Imagen')
+    idPublicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, verbose_name='Id publicacion')
+    nombre = models.TextField(max_length=20)
+    archivo = models.CharField(max_length=20)
+    
+    
+class Comentario(models.Model):
     idComentario = models.AutoField(primary_key=True,verbose_name='Codigo Comentario')
+    idPublicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, verbose_name='Id publicacion')
+    idUsuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name='Id usuario')
+    comentario = models.TextField(max_length=500)
+    fechaComentario = models.DateField(_(""), auto_now=False, auto_now_add=False)
+    estatus = models.CharField(max_length=50)
+    
+
+
+
+    
     
     

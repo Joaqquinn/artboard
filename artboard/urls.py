f6c636397cfb129525a80a66abd46fb6e0f54e0a
Joaqquinn <joaqu.gonzalezl@duocuc.cl>
2023-05-18 12:10:24 -0400

    creando inicio de base de datos en django y clase

--- artboard/urls.py
+++ artboard/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('publicaciones/',include('publicaciones.urls') )
-    
-    
+    path('publicaciones/',include('publicaciones.urls') ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- publicaciones/models.py
+++ publicaciones/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Pregunta(models.Model):
+    idPregunta = models.AutoField(primary_key=True,verbose_name='Codigo Pregunta')
+    descripcion = models.TextField()
+
+
+class Rol(models.Model):
+    idRol = models.AutoField(primary_key=True,verbose_name='Codigo Rol')
+    nombre = models.CharField(max_length=50)
+
+
+class Usuario(models.Model):
+    idUsuario = models.AutoField(primary_key=True,verbose_name='Codigo Usuario')
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    rol = models.ForeignKey(Rol,on_delete=models.CASCADE,verbose_name='Rol')
+    pregunta = models.ForeignKey(Pregunta,on_delete=models.CASCADE,verbose_name='Pregunta')
+
+
+class Comentario (models.Model):
+    idComentario = models.AutoField(primary_key=True,verbose_name='Codigo Comentario')
+    
+    

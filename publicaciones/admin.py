7484d1ee851e8433c315586640a17bad971513e3
Joaqquinn <joaqu.gonzalezl@duocuc.cl>
2023-05-25 11:59:24 -0400

    registra modelos

--- publicaciones/admin.py
+++ publicaciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Rol,Usuario,Publicacion,Imagen,Comentario
 
 # Register your models here.
+admin.site.register(rol)
+admin.site.register(Usuario)
+admin.site.register(Publicacion)
+admin.site.register(imagen)
+admin.site.register(comentario)

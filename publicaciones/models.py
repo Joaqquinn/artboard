f5bd9ccaad74d4d8fce989ecb6e146952f7c92f7
Joaqquinn <joaqu.gonzalezl@duocuc.cl>
2023-06-08 15:09:12 -0400

    black

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -7,55 +7,45 @@ from django.contrib.auth.models import User
 
 
 class Rol(models.Model):
-    idRol = models.AutoField(primary_key=True,verbose_name='Codigo Rol')
+    idRol = models.AutoField(primary_key=True, verbose_name="Codigo Rol")
     nombre = models.CharField(max_length=50)
 
 
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    fotoPerfil = models.ImageField(default='logo.png')
+    fotoPerfil = models.ImageField(default="logo.png")
 
     def __str__(self):
-        return f'{self.usuario.username}'
+        return f"{self.usuario.username}"
 
 
-
-
-
-    
-
-    
-    
 class Publicacion(models.Model):
-    idPublicacion = models.AutoField(primary_key=True,verbose_name='Codigo Publicacion')
+    idPublicacion = models.AutoField(
+        primary_key=True, verbose_name="Codigo Publicacion"
+    )
     titulo = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=500)
     fechaPublicacion = models.DateField(auto_now=False, auto_now_add=False)
     estatus = models.CharField(max_length=50)
-    usuario = models.ForeignKey(User,null=True, on_delete=models.CASCADE, verbose_name='Usuario')
-    
-    
+    usuario = models.ForeignKey(
+        User, null=True, on_delete=models.CASCADE, verbose_name="Usuario"
+    )
+
+
 class Imagen(models.Model):
-    idImagen = models.AutoField(primary_key=True,verbose_name='Codigo Imagen')
-    idPublicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, verbose_name='Id publicacion')
+    idImagen = models.AutoField(primary_key=True, verbose_name="Codigo Imagen")
+    idPublicacion = models.ForeignKey(
+        Publicacion, on_delete=models.CASCADE, verbose_name="Id publicacion"
+    )
     nombre = models.TextField(max_length=20)
     archivo = models.CharField(max_length=20)
-    
-    
+
+
 class Comentario(models.Model):
-    idComentario = models.AutoField(primary_key=True,verbose_name='Codigo Comentario')
-    idPublicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, verbose_name='Id publicacion')
+    idComentario = models.AutoField(primary_key=True, verbose_name="Codigo Comentario")
+    idPublicacion = models.ForeignKey(
+        Publicacion, on_delete=models.CASCADE, verbose_name="Id publicacion"
+    )
     comentario = models.TextField(max_length=500)
     fechaComentario = models.DateTimeField(auto_now_add=True)
     estatus = models.CharField(max_length=50)
-
-
-
-
-
-
-
-
-    
-    
-    
